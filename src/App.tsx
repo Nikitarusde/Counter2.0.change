819d2b8251b10241998a66490fe669fc51ff0168
Nikita <nikistosov@gmail.com>
2022-01-31 13:36:18 +0100

    counter2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
+import {Setting} from "./components/setting/Setting";
+import {Counter} from "./components/counter/Counter";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [counter, setCounter] = useState(0)
+
+    const [startCounter, setStartCounter] = useState(0)
+
+    const [endCounter, setEndCounter] = useState<number>(0)
+
+    const [flag, setFlag] = useState(true)
+
+    const changeCounter = () => {
+        setFlag(false)
+    }
+
+    useEffect(() => {
+        let counterValueStart = localStorage.getItem("counterValue")
+        if (counterValueStart) {
+            let newCounterValueStart = JSON.parse(counterValueStart)
+            setStartCounter(newCounterValueStart)
+            setCounter(newCounterValueStart)
+        }
+    }, [])
+
+    useEffect(() => {
+        let counterValueEnd = localStorage.getItem("counterValueEnd")
+        if (counterValueEnd) {
+            let newCounterValueStart = JSON.parse(counterValueEnd)
+            setEndCounter(newCounterValueStart)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("counterValue", JSON.stringify(startCounter))
+    }, [startCounter])
+
+    useEffect(() => {
+        localStorage.setItem("counterValueEnd", JSON.stringify(endCounter))
+    }, [endCounter])
+
+
+    const onClickHandler = () => {
+        if (counter < endCounter)
+            setCounter(counter + 1)
+    }
+
+    const onClickHandlerDel = () => {
+        setCounter(startCounter)
+    }
+
+
+    const onChangeCounterStart = (e: ChangeEvent<HTMLInputElement>) => {
+        setStartCounter(+e.currentTarget.value)
+    }
+
+    const onChangeCounterEnd = (e: ChangeEvent<HTMLInputElement>) => {
+        setEndCounter(+e.currentTarget.value)
+    }
+
+    const onClickHandlerSet = () => {
+        setCounter(startCounter)
+        setFlag(true)
+    }
+
+    return (
+
+
+        <div className="App">
+            {flag ? (
+                <Counter
+                    changeCounter={changeCounter}
+                    endCounter={endCounter}
+                    counter={counter}
+                    onClickHandler={onClickHandler}
+                    onClickHandlerDel={onClickHandlerDel}
+                    startCounter={startCounter}
+                />
+            ) : (
+                <Setting
+                    startCounter={startCounter}
+                    endCounter={endCounter}
+                    onChangeCounterStart={onChangeCounterStart}
+                    onChangeCounterEnd={onChangeCounterEnd}
+                    onClickHandlerSet={onClickHandlerSet}
+                />
+            )}
+
+
+        </div>
+
+    );
 }
 
 export default App;
